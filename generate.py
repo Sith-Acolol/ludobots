b4f7d69e640daee140da98375c5a10dd9dc9f8cb
Sith-Acolol <samfueler@gmail.com>
2023-11-22 00:46:32 -0600

    created a hook shaped robot from 4 links and 3 joints

--- generate.py
+++ generate.py
@@ -1,16 +1,28 @@
 import pyrosim.pyrosim as pyrosim
+
 length = 1
 width = 1
 height = 1
 x = 0
 z = 0
 y = 0.5
-pyrosim.Start_SDF("boxes.sdf")
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length,width,height])
-    y = y + height/2
-    length = length * 0.9
-    width = width * 0.9
-    height = height * 0.9
-    y = y + height/2
-pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x-2,z+2,y], size=[length,width,height])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[x,z,y], size=[length,width,height])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[x,z,y+0.5])
+    pyrosim.Send_Cube(name="Link1", pos=[x,z,y], size=[length,width,height])
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[x,z,y+0.5])
+    pyrosim.Send_Cube(name="Link2", pos=[x,z,y], size=[length,width,height])
+    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[x+0.5,z,y])
+    pyrosim.Send_Cube(name="Link3", pos=[x+1,z,y], size=[length,width,height])
+    
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -1,7 +1,12 @@
+import pybullet_data
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
-p.loadSDF("boxes.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
