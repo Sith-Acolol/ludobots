9698f369a1ba93d67a3a611aea70b44cc87d202c
Sith-Acolol <samfueler@gmail.com>
2023-12-26 22:11:27 -0600

    finished the slide deck

--- generate.py
+++ generate.py
@@ -20,9 +20,15 @@ def Create_Robot():
     pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[x,z,y+0.5])
     pyrosim.Send_Cube(name="Link2", pos=[x,z,y], size=[length,width,height])
     pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[x+0.5,z,y])
-    pyrosim.Send_Cube(name="Link3", pos=[x+1,z,y], size=[length,width,height])
-    
+    pyrosim.Send_Cube(name="Link3", pos=[x+0.5,z,y-0.5], size=[length,width,height])
+    pyrosim.Send_Joint(name="Link3_Link4", parent="Link3", child="Link4", type="revolute", position=[x+1,z,y-0.5])
+    pyrosim.Send_Cube(name="Link4", pos=[x+0.5,z,y-0.5], size=[length,width,height])
+    pyrosim.Send_Joint(name="Link4_Link5", parent="Link4", child="Link5", type="revolute", position=[x+0.5,z,y-1])
+    pyrosim.Send_Cube(name="Link5", pos=[x,z,y-1], size=[length,width,height])
+    pyrosim.Send_Joint(name="Link5_Link6", parent="Link5", child="Link6", type="revolute", position=[x,z,y-1.5])
+    pyrosim.Send_Cube(name="Link6", pos=[x,z,y-1], size=[length,width,height])
+
     pyrosim.End()
 
-Create_World()
+# Create_World()
 Create_Robot()
